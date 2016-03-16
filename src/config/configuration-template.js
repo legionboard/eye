8d4429886eef2d41e6ef7c7f66bb116199dda996
Nico Alt <nicoalt@posteo.org>
2016-03-16 19:26:03 +0100

    Add configuration

---
+++ src/config/configuration-template.js
@@ -0,0 +1,15 @@
+/*
+ * This is the configuration file of the LegionBoard JavaScript client.
+ * Copy this file or rename it to "configuration.js".
+ *
+ * @author	Nico Alt
+ * @date	16.03.2016
+ *
+ * See the file "LICENSE" for the full license governing this code.
+ */
+var appConfig = {
+	// Root URL of LegionBoard REST API (no slash at the end)
+	'apiRoot': 'https://api.legionboard.example.com',
+	// Title for LegionBoard
+	'title': 'LegionBoard'
+};
