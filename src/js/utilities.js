16e9392b872bec7d1e5514ae0cc612ade6d9b21b
Nico Alt <nicoalt@posteo.org>
2016-08-06 22:11:54 +0200

    Bump to 0.2.0-beta2

--- src/js/utilities.js
+++ src/js/utilities.js
@@ -6,7 +6,7 @@
  */
 var title = appConfig['title'];
 setTitle();
-var version = '0.2.0-beta1';
+var version = '0.2.0-beta2';
 setVersion();
 // Prevent caching in ajax calls
 $.ajaxSetup({ cache: false });
